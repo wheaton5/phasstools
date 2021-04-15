d16cafe16d9e0e3eb2009b6f4f3a4b25385bfca0
wheaton5 <whheaton@gmail.com>
2021-04-15 16:46:12 -0400

    list'

--- phasstools.py
+++ phasstools.py
@@ -206,13 +206,15 @@ def het_kmer_molecules_FASTK(cutoffs):
             ccs_files.append(line.split())
     print("running fastk profiles on phasemer.U")
     cmd = [directory + "/FASTK/FastK", "-k"+str(args.kmer_size), "-p:"+args.output+"/phasemer.U", 
-        "-N"+args.output+"/phasemap.U"] + ccs_files
+        "-N"+args.output+"/phasemap.U"]
+    cmd.extend(ccs_files)
     print(cmd)
     print(" ".join(cmd))
     check_call(cmd, "fastk_phasemer.U")
     print("running fastk profiles on phasemer.L")
     cmd = [directory + "/FASTK/FastK", "-k"+str(args.kmer_size), "-p:"+args.output+"/phasemer.L", 
-        "-N"+args.output+"/phasemap.L"] + ccs_files 
+        "-N"+args.output+"/phasemap.L"]
+    cmd.extend(ccs_files)
     print(" ".join(cmd))
     check_call(cmd, "fastk_phasemer.L")
     print("running phasemap to combine phasemer.U and phasemer.L")
